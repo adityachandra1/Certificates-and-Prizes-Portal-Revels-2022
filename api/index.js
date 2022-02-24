a90533db6772dbdb5de014dd781797e3cae9268e
Aditya Chandra <adityachandra9898@gmail.com>
2022-02-24 19:31:43 +0530

    added-middlewares

--- api/index.js
+++ api/index.js
@@ -7,6 +7,9 @@ const app = express();
 const DB_URI = 'mongodb+srv://admin:admin123@cluster0.gqru4.mongodb.net/cnp-portal?retryWrites=true&w=majority'
 const PORT = 8080;
 
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 mongoose.connect(DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -15,7 +18,7 @@ mongoose.connect(DB_URI, {
   .then(console.log("Connected to DB\nListening to port " + PORT))
   .catch((err) => console.log(err));
 
-  
+
 app.get("/", function (req, res) {
   res.send("Great World!");
 });
