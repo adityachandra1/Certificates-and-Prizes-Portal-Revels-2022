3c7df55210520dbfdb38b8af64954d5cdf5a4a66
Aditya Chandra <adityachandra9898@gmail.com>
2022-02-26 20:35:02 +0530

    updated-schema

---
+++ api/models/adminModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['OPERATION', 'SC', 'OM', 'JUDGES', 'VIGILANCE', 'CNP', 'INF', 'CATEGORY'],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  token: {
+    type: String,
+  },
+  // Only for Category type
+  events: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Event',
+    },
+  ],
+});
+
+module.exports = Admin = mongoose.model('Admin', adminSchema);
