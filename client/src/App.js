f2ec2a6cf7e297401f8f29a6c014a5085841ad26
Chintan0539 <chintanpatel0539@gmail.com>
2022-03-08 09:20:19 +0530

    subdomain done mockup 7

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import "./App.css";
 import Login from "./pages/login.page";
 import { Routes, Route } from "react-router-dom";
 import Event from "./pages/Event.page";
-import Preview from './pages/Preview.page';
-import Domain from './pages/domain.page';
+import Preview from "./pages/Preview.page";
+import Domain from "./pages/domain.page";
 import Upload from "./pages/Upload.page";
+import Subdomain from "./pages/SubDomain.page";
 
 function App() {
   return (
     <div className="App">
-      
       {/* Routes */}
       <Routes>
         <Route exact path="/" element={<Login />} />
@@ -17,6 +17,7 @@ function App() {
         <Route exact path="/domain" element={<Domain />} />
         <Route exact path="/certificates-preview" element={<Preview />} />
         <Route exact path="/upload-winners" element={<Upload />} />
+        <Route exact path="/subdomain" element={<Subdomain />} />
       </Routes>
     </div>
   );
