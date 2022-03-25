9d733cf1347098e06cc2fab1e7b360450b23e1b3
Aditya Chandra <adityachandra9898@gmail.com>
2022-03-25 21:47:06 +0530

    mailing-integrated

--- api/routes/certificateRoutes.js
+++ api/routes/certificateRoutes.js
@@ -7,6 +7,8 @@ const puppeteer = require('puppeteer');
 const email_list = require('../uploads/email_list.json');
 const Certificate = require('../models/certModel');
 const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+
 
 const compile = async function (templateName, data) {
     const filePath = path.join(process.cwd(), 'templates', `${templateName}`.hbs);
@@ -15,6 +17,16 @@ const compile = async function (templateName, data) {
 }
 
 router.post('/cert', async (req, res) => {
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'adityachandra.dev.testing@gmail.com',
+            //put your email id for testing
+            pass: "Testing*1",
+        }
+    });
+
     for (var i in email_list) {
         try {
             const browser = await puppeteer.launch();
@@ -37,6 +49,29 @@ router.post('/cert', async (req, res) => {
                 printBackground: true
             })
             console.log("Certificate " + i + " generated!");
+
+            const mailOptions = {
+                from: 'adityachandra.dev.testing@gmail.com',
+                to: email_list[i].email,
+                subject: 'CNP Certificate',
+                text: 'CNP-Mailer testing',
+                attachments: [
+                    {
+                        filename: email_list[i].name + '.pdf',
+                        contentType: 'application/pdf',
+                        path: './certificates/' + token + '.pdf',
+                    },]
+            };
+
+            transporter.sendMail(mailOptions, (err, data) => {
+                if (err) {
+                    console.log("Email Not Sent!", err);
+                    return;
+                } else {
+                    console.log(data, "EMAIL SENT!")
+                }
+            });
+
         } catch (e) {
             console.log(e);
             res.status(300).json("Certificate" + i + "Not Generated");
@@ -44,32 +79,5 @@ router.post('/cert', async (req, res) => {
     }
     res.status(200).json('ALL CERTIFICATES GENERATED');
 })
-
-//USING PDF-LIB
-// const ORGANISER_CERT_X = 50;
-// const ORGANISER_CERT_Y = 600;
-// router.post("/certificate", async(res, req) => {
-//     const existingPDFBytes = fs.readFile('./certificates/sample_cert.pdf', 'Uint8Array' , (err, data)=>{
-//         console.log(data);
-//     });
-
-//     const pdfDoc = await PDFDocument.load(existingPDFBytes);
-
-//     const pages = pdfDoc.getPages;
-//     for (var i in email_list) {
-//         const cert = pages[0];
-//         const { width, height } = cert.getSize();
-
-//         const fontSize = 45;
-//         page.drawText(email_list[i].name, {
-//             x: width/2,
-//             y: height - 4 * fontSize,
-//             size: fontSize,
-//             font: timesRomanFont,
-//             color: rgb(1, 1, 0),
-//         })
-//         const pdfBytes = await pdfDoc.save();
-//     }
-// });
 
 module.exports = router;
--- api/routes/mailingRoutes.js
+++ api/routes/mailingRoutes.js
@@ -7,38 +7,11 @@ const email_list = require('../uploads/email_list.json');
 require('dotenv').config()
 
 router.post('/send-mail', (req, res) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'adityachandra.dev.testing@gmail.com',
-            //put your email id for testing
-            pass: process.env.PASSWORD,
-        }
-    });
-
     let mailing_list = [];
     for(var i in email_list){
         mailing_list.push(email_list[i].email);
     }
-
     console.log(mailing_list);
-
-    const mailOptions = {
-        from: 'adityachandra.dev.testing@gmail.com',
-        to: mailing_list,
-        subject: 'CNP TESTING',
-        text: 'CNP-Mailer testing'
-    };
-
-    transporter.sendMail(mailOptions, (err, data) => {
-        if (err) {
-            console.log("Email Not Sent!", err);
-            return;
-        } else {
-            console.log(data, "EMAIL SENT!")
-            res.json({ status: "success", error: "", data: "" });
-        }
-    });
 })
 
 
